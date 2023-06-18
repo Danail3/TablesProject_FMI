e4c7e8b224c367f08951ae5cad41545150b65ddd
Danail3 <96911512+Danail3@users.noreply.github.com>
2023-06-19 00:12:57 +0300

    Update TablesProject_FMI.cpp

--- TablesProject_FMI/TablesProject_FMI.cpp
+++ TablesProject_FMI/TablesProject_FMI.cpp
@@ -1,37 +1,7 @@
-//#include <iostream>
-//#include "GlobalConstants.h"
-//#include "Value.h"
-#include "Table.h"
-#include <fstream>
-#include "myVector.hpp"
+#include "Menu.h"
+//Using MSVC compiler
 
 int main()
 {
-	Value value2("2");
-	Value value3("3");
-	Value value4("4");
-	Value value5("5");
-	Value value6("6");
-	Vector<Value> surr{5};
-	surr.pushBack(value6);
-	surr.pushBack(value5);
-	surr.pushBack(value4);
-	surr.pushBack(value3);
-	surr.pushBack(value2);
-	Row newRow(surr);
-	Row newRow1(newRow);
-	Vector<Row> rows{2};
-	rows.pushBack(newRow);
-	rows.pushBack(newRow1);
-	Table table(rows);
-
-	const char* name = "test.txt";
-	std::fstream myFile(name, std::ios::in | std::ios::out | std::ios::app);
-	Table myTable; 
-	myTable.exportDataFromFile(myFile);
-	std::fstream NOVO("NOVTESTAHAH.txt", std::ios::in | std::ios::out | std::ios::app);
-	myTable.saveDataToFile(NOVO);
-	myTable.editCell(1, 0, "136");
-	myTable.editCell(1, 1, "136");
-	myTable.saveDataToFile(NOVO);
+	Menu::runProgram();
 }
