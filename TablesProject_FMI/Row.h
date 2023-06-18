e9f26b52d977ece0c829095381c6a962fb184cb2
Danail3 <96911512+Danail3@users.noreply.github.com>
2023-06-19 00:12:16 +0300

    Update Row.h

--- TablesProject_FMI/Row.h
+++ TablesProject_FMI/Row.h
@@ -4,21 +4,33 @@
 #include "myVector.hpp"
 
 class Row {
-	Vector<Value> values;
+	Value** values;
+	size_t valuesCount;
+	size_t capacity;
+
+	void addValue(const MyString& str);
+	bool checkForMissedCommas(const MyString& str, char& symbolForComma);
+	bool checkIfFormulaExists(std::stringstream& ss, unsigned& begInd, unsigned& endInd);
+
+	void free();
+	void copyFrom(const Row& other);
+	void moveFrom(Row&& other);
+	void resize();
 
 public:
-	Row() = default;
-	Row(const Vector<Value> newValues);
+	Row();
+	Row(const Row& other);
+	Row& operator=(const Row& other);
+	~Row();
+	Row(Row&& other) noexcept;
+	Row& operator=(Row&& other) noexcept;
 
-	void setValues(const Vector<Value> values);
-	void printRow() const; //shte mi trqq za priniraneto na tablicata
-	const Vector<Value>& getValues() const;
+	const size_t getValuesCount() const;
 
-	void readRow(std::fstream& fs);
+	void readRow(std::fstream& fs, size_t& rowIndex);
 	void saveRow(std::fstream& fs) const;
 
-	Value& operator[](int index);
-	Value operator[](int index) const; //tuka nz dali trqq da e taka
+	Value* operator[](int index);
+	const Value* operator[](int index) const;
 };
-//void openFile(const MyString& name);
 
